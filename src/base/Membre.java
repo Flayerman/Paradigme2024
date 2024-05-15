a523b4f7aab589eeb68426e0699fceca9836de30
Antoine ENGASSER <antoine.engasser@edu.esiee-it.fr>
2024-05-15 15:35:12 +0200

    Exo2

--- src/base/Membre.java
+++ src/base/Membre.java
@@ -1,7 +1,18 @@
 package base;
 
 public class Membre extends Personne {
+	public String dateAdhesion;
+	public String statut;
+
 	public Membre(String nom, String prénom, int id, String dateAdhesion, String statut) {
 		super(nom, prénom, id);
+		this.dateAdhesion = dateAdhesion;
+		this.statut = statut;
+	}
+
+	public void AfficherMembre() {
+		afficherDetails();
+		System.out.println("Date d'adhésion : " + this.dateAdhesion);
+		System.out.println("Statut : " + this.statut);
 	}
 }
